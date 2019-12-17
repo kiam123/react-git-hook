f20d395a9a68010e6f81eeb6c85398a0e4c293e9
kiam <kiam125@gmail.com>
2019-12-17 21:57:46 +0800

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React WTF  34245 789  12 TED   123123123
+          Learn React WTF  34245 789  12 TE
         </a>
       </header>
     </div>
