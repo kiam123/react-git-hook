9e1fc96e9410c04aa858564182e077036601702a
kiam <kiam125@gmail.com>
2019-12-19 13:44:00 +0800

    123123

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React WTF  34245 789  12 TEDDDD WTF a asd1  123123
+          Learn React WTF  34245 789  12 TEDDDD WTF a asd1 
         </a>
       </header>
     </div>
